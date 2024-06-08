91bc759ab99fa8574eb9a739791814b4bdea307f
Geonseob Kim <gnksbm@gmail.com>
2024-06-08 23:51:13 +0900

    검색 필드 / 버튼 추가, UII 수정

--- MovieAPI/Sources/View/ViewController.swift
+++ MovieAPI/Sources/View/ViewController.swift
@@ -17,6 +17,41 @@ final class ViewController: UIViewController {
         }
     }
     
+    private lazy var datePicker = {
+        let pickerView = UIDatePicker()
+        pickerView.preferredDatePickerStyle = .wheels
+        pickerView.datePickerMode = .date
+        return pickerView
+    }()
+    
+    private lazy var textField = {
+        let textField = UITextField()
+        textField.placeholder = "날짜를 선택해주세요"
+        textField.text = API.dailyBoxOffice.yesterday
+        textField.inputView = datePicker
+        textField.font = .systemFont(ofSize: 20, weight: .medium)
+        textField.addTarget(
+            self,
+            action: #selector(fetchButtonTapped),
+            for: .editingDidEndOnExit
+        )
+        return textField
+    }()
+    
+    private lazy var searchButton = {
+        let button = UIButton(configuration: .filled())
+        button.configuration?.cornerStyle = .small
+        button.configuration?.baseForegroundColor = .systemBackground
+        button.configuration?.baseBackgroundColor = .label
+        button.setTitle("검색", for: .normal)
+        button.addTarget(
+            self,
+            action: #selector(fetchButtonTapped),
+            for: .touchUpInside
+        )
+        return button
+    }()
+    
     private lazy var tableView = {
         let tableView = UITableView()
         tableView.separatorStyle = .none
@@ -51,17 +86,46 @@ final class ViewController: UIViewController {
         }
     }
     
+    @objc private func fetchButtonTapped() {
+        if let url = API.dailyBoxOffice.getURL(date: datePicker.date) {
+            AF.request(url)
+                .responseDecodable(
+                    of: MovieData.self
+                ) { [weak self] response in
+                    guard let self else { return }
+                    switch response.result {
+                    case .success(let movieData):
+                        list = movieData.dailyBoxOfficeList
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
+        }
+    }
+    
     private func configureUI() {
         view.backgroundColor = .systemBackground
         
-        [tableView].forEach {
+        [textField, searchButton, tableView].forEach {
             view.addSubview($0)
         }
         
         let safeArea = view.safeAreaLayoutGuide
         
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(safeArea)
+            make.leading.equalTo(safeArea).offset(20)
+            make.trailing.equalTo(searchButton.snp.leading).offset(-20)
+        }
+        
+        searchButton.snp.makeConstraints { make in
+            make.top.equalTo(textField)
+            make.trailing.equalTo(safeArea).offset(-20)
+        }
+        
         tableView.snp.makeConstraints { make in
-            make.edges.equalTo(safeArea)
+            make.top.equalTo(textField.snp.bottom).offset(20)
+            make.leading.trailing.bottom.equalTo(safeArea)
         }
     }
 }
